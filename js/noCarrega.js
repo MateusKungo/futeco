68870aec5002c0548a098b687a1d394d4096a595
João Silva <joao.silva@example.com>
2024-10-23 20:28:35 +0100

    subir

--- js/noCarrega.js
+++ js/noCarrega.js
@@ -1,12 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Aqui você pode adicionar a lógica de autenticação ou exibir mensagens de erro/sucesso
-    console.log('Login formulário enviado');
- });
-
- // Impede o recarregamento da página ao submeter o formulário de Registro
- document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Aqui você pode adicionar a lógica de registro ou exibir mensagens de erro/sucesso
-    console.log('Registro formulário enviado');
- });
+var currentYear = new Date().getFullYear();
+   
+// Insere o ano no elemento com o ID 'currentYear'
+document.getElementById("currentYear").textContent = currentYear;
